4e60c7fa181ee2584d862c17023dcd2fa447d3b9
sanchezmg-cr <131888545+sanchezmg-cr@users.noreply.github.com>
2023-05-15 17:18:05 -0600

    push 17:18

---
+++ 02-htpp-Verbs-Web-Api/02-htpp-Verbs-Web-Api/Controllers/HttpVerbsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_htpp_Verbs_Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HttpVerbsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get() {
+            //
+            return Ok("Metodo Get");
+        }
+
+        
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return Ok("Metodo Post");
+        }
+
+        [HttpPut]
+        public ActionResult Put()
+        {
+            return Ok("Metodo Put");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return Ok("Metodo Delete");
+        }
+
+        // parcialmente un registro
+        [HttpPatch]
+        public ActionResult Patch()
+        {
+            return Ok("Metodo Patch");
+        }
+    }
+
+
+}
