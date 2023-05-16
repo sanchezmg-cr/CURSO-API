dfaf3bc5cc2dd7011ea14b79f51a21134df7c4fe
Gonzalo Sanchez <sanchezmg.cr@gmail.com>
2023-05-16 14:18:49 -0600

    14:18

--- 03-Routing-Web-Api/03-Routing-Web-Api/DTO/Class.cs
+++ 03-Routing-Web-Api/03-Routing-Web-Api/DTO/Class.cs
@@ -9,6 +9,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
+        public string otro { get; set; }
         
     }
 
