8edc76a85d202f369043661c9433febbf48f0389
Gonzalo Sanchez <sanchezmg.cr@gmail.com>
2023-05-16 15:40:48 -0600

    commit 15:40

--- 03-Routing-Web-Api/03-Routing-Web-Api/DTO/EmployeeDTO.cs
+++ 03-Routing-Web-Api/03-Routing-Web-Api/DTO/EmployeeDTO.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Position { get; set; }
+        public string Address { get; set; }
+
+        
     }
 }
